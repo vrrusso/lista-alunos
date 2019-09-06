ff628e4542365442a6a3a9b37289857be676902f
Victor R. Russo <vr.russo@usp.br>
2019-09-06 14:49:15 -0300

    terminar a main

--- teste.c
+++ teste.c
@@ -4,58 +4,78 @@
 
 int menu(){ // menu dando erro quando se digita
 	int op = 0;
-
+    char aux;
 	do{
 		printf("-------------MENU-------------\n");
-		printf("| 1 - Inserir Registro\n| 2 - Remover Registro\n| 3 - Imprimir Registros\n| 4 - Relatório de Aprovação\n| 5 - Horas de estudo (Média)\n| 6 - Sair\n");
+		printf("| 1 - Inserir Registro       |");
+		printf("\n| 2 - Remover Registro       |");
+		printf("\n| 3 - Imprimir Registros     |");
+        printf("\n| 4 - Relatório de Aprovação |");
+		printf("\n| 5 - Horas de estudo(Média) |");
+        printf("\n| 6 - Sair                   |\n");
 		printf("------------------------------\n");
 		printf("ecolha uma opção: ");
-
-		scanf("%d", &op);
-
-		if(op>=1 && op<=6) return op;
-	 	else{
-			op = 0;
-			printf("Opção Invalida\n");
-		}
+        scanf(" %c",&aux);;
+		//scanf("%d", &op);
+        op=aux-'0';
+        if(op<1||op>6)
+            printf("\n\n\nOpção Inválida!!!\n\n\n\n");
 	}while(op<1 || op>6);
+	return op;
 }
-
+boolean  entrada_dados(Lista * lista)
+{
+    Aluno * aluno_aux = aluno_criar();
+    int id;
+    float horas,nota1,nota2;
+    printf("Id do Aluno:");scanf("%d",&id); 
+    printf("Horas de Estudo:");scanf("%f",&horas);
+    printf("Nota da Primeira Avaliação:");scanf("%f",&nota1);
+    printf("Nota da Segunda Avaliação:");scanf("%f",&nota2);
+    aluno_set(aluno_aux,id,horas,nota1,nota2);
+    boolean teste = lista_inserir(lista,aluno_aux);
+    aluno_apagar(aluno_aux);
+    return teste;
+}
 int main(void){
-//  	boolean flag = TRUE;
-//
-//  	while(flag){
-// 		switch (menu()){
-//
-// 			case (1):
-// 				printf("Opção - 1\n");
-// 			break;
-//
-// 			case (2):
-// 				printf("Opção - 2\n");
-// 			break;
-//
-// 			case (3):
-// 				printf("Opção - 3\n");
-// 			break;
-//
-// 			case (4):
-// 				printf("Opção - 4\n");
-// 			break;
-//
-// 			case (5):
-// 				printf("Opção - 5\n");
-// 			break;
-//
-// 			case (6):
-// 				flag = FALSE;
-// 			break;
-// 		}
-// 	}
+    int opcao;
+    Lista * lista = lista_criar();
+    do
+    {
+ 		switch (opcao = menu()){
 
+ 			case (1):
+ 				if(entrada_dados(lista)==1)
+                    printf("Aluno inserido com sucesso no fim da lista!\n");
+ 			break;
+
+ 			case (2):
+ 				printf("Opção - 2\n");
+ 			break;
+
+ 			case (3):
+ 			    printf("Todos os registros:\n");
+                lista_imprimir(lista);	
+ 			break;
+
+ 			case (4):
+ 				printf("Opção - 4\n");
+ 			break;
+
+ 			case (5):
+ 				printf("Opção - 5\n");
+ 			break;
+
+ 			default:	
+ 				printf("Obrigado por utilizar nosso programa!!\n");
+ 			break;
+ 		}
+    }while(opcao != 6);
+    lista_apagar(lista);
 
 
 //teste:
+/*
 	Aluno * aluno = aluno_criar();
 	Lista * lista=lista_criar();
 	int id;
@@ -75,6 +95,6 @@ int main(void){
 	lista_imprimir(lista);
 	lista_apagar(lista);
 
-
+*/
 	return 0;
 }
